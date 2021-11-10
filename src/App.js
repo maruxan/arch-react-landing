f05744f2473b35255efac8cd1914f3dd2f97d78f
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-10 13:13:43 -0300

    feat(project): react-router-dom v6 routes setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Fragment } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
-  return <div>Arch Landing</div>;
+  return (
+    <Fragment>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element="Home" />
+        <Route path="/portfolio" element="Portfolio" />
+        <Route path="/about" element="About Us" />
+        <Route path="/contact" element="Contact" />
+        <Route path="*" element="404" />
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
