ab8c5a09780d09bff58208ccfce726b7282ef181
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-10 13:30:55 -0300

    feat(pages): created empty page components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Fragment } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 function App() {
   return (
     <Fragment>
       <Routes>
         <Route exact path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element="Home" />
-        <Route path="/portfolio" element="Portfolio" />
-        <Route path="/about" element="About Us" />
-        <Route path="/contact" element="Contact" />
+        <Route path="/home" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element="404" />
       </Routes>
     </Fragment>
