b3b87e50ab0db82d6fe7fd4acac631b7f51d4698
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-12 10:51:33 -0300

    feat(portfolio): created portfolio page component

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+import projects from '../../data/projects';
+import classes from './Portfolio.module.css';
+
+import PageMarker from '../../components/PageMarker/PageMarker';
+import Cards from '../../components/Cards/Cards';
+import ProjectCard from '../../components/Cards/ProjectCard/ProjectCard';
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <main className={classes.portfolio}>
+      <PageMarker page="portfolio" />
+      <section>
+        <Cards>
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              name={project.name}
+              subtitle={project.date}
+              img={project.image}
+              onclick={() => undefined}
+            />
+          ))}
+        </Cards>
+      </section>
+    </main>
+  );
 }
